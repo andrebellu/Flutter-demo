d0c611eb9780c6dbb79b0bc696d616b00ec29d5a
andrebellu <bellu.andrea@outlook.it>
2023-05-03 20:46:58 +0200

    added ui mode

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,12 @@
 import 'package:english_words/english_words.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:provider/provider.dart';
 
 void main() {
   runApp(MyApp());
+
+  SystemChrome.setEnabledSystemUIMode(SystemUiMode.edgeToEdge);
 }
 
 class MyApp extends StatelessWidget {
